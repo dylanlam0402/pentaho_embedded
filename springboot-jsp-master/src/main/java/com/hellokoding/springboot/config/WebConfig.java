8d7677534d345f452b382f6568e702cb29cbe87b
congle <congle@kms-technology.com>
2017-09-07 15:58:44 +0700

    Merge branch 'master' of https://github.com/dylanlam0402/pentaho_embedded
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ springboot-jsp-master/src/main/java/com/hellokoding/springboot/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.hellokoding.springboot.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by kietlam on 9/7/2017.
+ */
+@Configuration
+public class WebConfig {
+    @Bean
+    public ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+        registration.addUrlMappings("/h2/*");
+        return registration;
+    }
+}
---
+++ springboot-jsp-master/src/main/resources/db/migration/V1.01__db_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users(
+  id IDENTITY,
+  email VARCHAR(100) NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  password VARCHAR(255)NOT NULL,
+  role VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
+
